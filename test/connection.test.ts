300c2bdd9564fb85f377da09f54cb80acad7ac87
JeffWScott <jeffscott@outlook.com>
2019-10-29 15:37:57 -0400

    remove files that do not compile

--- test/connection.test.ts
+++
@@ -1,8 +0,0 @@
-import * as connection from '../build/ts/connection';
-import * as assert from 'assert';
-import * as testhelpers from './testhelpers';
-import * as helpers from '../build/ts/helpers';
-import * as chai from 'chai';
-
-describe('Connection', function () {
-})
